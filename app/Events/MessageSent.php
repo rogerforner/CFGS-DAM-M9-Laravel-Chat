f84a1689a696697efaab0cc88c2a4fc0a4293c5f
Roger Forner Fabre <hello@rogerforner.com>
2017-03-27 23:19:26 +0200

    Iniciem la pràctica "chat" amb AdminLTE

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Events;
+
+use App\Message;
+use App\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+/**
+ * Class MessageSent.
+ *
+ * @package App\Events
+ */
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+
+    public $message;
+
+    /**
+     * MessageSent constructor.
+     * @param $user
+     * @param $message
+     */
+    public function __construct(User $user, Message $message)
+    {
+        $this->user = $user;
+        $this->message = $message;
+    }
+
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('chat');
+    }
+}
