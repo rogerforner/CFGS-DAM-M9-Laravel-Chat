fc8b0ceeb59a2fa493e907b8f8d3da3953b05094
Roger Forner Fabre <hello@rogerforner.com>
2017-03-28 00:45:22 +0200

    Redis

--- server/server.js
+++ server/server.js
@@ -5,8 +5,19 @@ var Redis = require('ioredis');
 
 var redis = new Redis();
 
+redis.psubscribe('*', function(err, count) {
+    console.log('Errors subscribing to channel');
+});
+
+redis.on('pmessage', function(subscribed,channel, message) {
+    console.log('Message Recieved at channel(' + channel + '): ' + message);
+    message = JSON.parse(message);
+    io.emit(channel + ':' + message.event, message.data);
+});
+
+
 http.listen(3000,function () {
 
-    console.log("listening at port 3000")
+    console.log("listening at port 3000");
 
 });
